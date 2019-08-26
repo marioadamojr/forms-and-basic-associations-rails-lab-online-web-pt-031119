fe8332bd4540d717c4cd240dbd80d80185a81a1a
Mario Adamo Jr <marioadamojr@gmail.com>
2019-08-26 00:02:53 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,30 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist
+  belongs_to :genre
+
+  def artist_name=(a_name)
+    artist = Artist.find_or_create_by(name: a_name)
+    self.artist = artist
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def genre_name=(g_name)
+    genre = Genre.find_or_create_by(name: g_name)
+    self.genre = genre
+  end
+
+  def genre_name
+    self.genre ? self.genre.name : nil
+  end
+
+  def note_contents=(s_name)
+  end
+
+  def note_contents
+  end
+
 end
